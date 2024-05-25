cec12a206e7d1e00ee23607eb331b3e9b59ed9e4
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-26 02:20:20 +0300

    update popular-experience section and popular-experience-card component.

--- app/(root)/(routes)/(home)/_components/popular-experiences.tsx
+++ app/(root)/(routes)/(home)/_components/popular-experiences.tsx
@@ -1,77 +1,37 @@
 "use client";
 
-import Link from "next/link";
-import Autoplay from "embla-carousel-autoplay";
-
+import { Key } from "react";
 import { RootState } from "@/lib/store";
 import { useSelector } from "react-redux";
-import { ArrowUpRight, TriangleAlert } from "lucide-react";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-import {
-  PopularExperienceCard,
-  PopularExperienceCardSkeleton,
-} from "@/components/card/popular-experience-card";
-import { Button } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
+import { PopularExperienceCard } from "@/components/card/popular-experience-card";
+import { SkeletonCard } from "@/components/card/skeleton-card";
+import { SectionCarousel } from "@/components/carousel/section-carousel";
+import { CarouselItem } from "@/components/ui/carousel";
+
+import { PopularExperience } from "@/interface";
 
 export const PopularExperiences = () => {
   const { data, status } = useSelector((state: RootState) => state.data);
 
   return (
-    <div className="py-10 container mx-auto">
-      <Heading title="Rehlatiuae Popular Experiences">
-        <Button className="px-0 tracking-wide" variant="link" asChild>
-          <Link href="/popular-experiences">
-            see all <ArrowUpRight className="h-5 w-5 ml-2" />
-          </Link>
-        </Button>
-      </Heading>
-      {status === "loading" && (
-        <div className="mt-10 grid grid-flow-col grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-5">
-          <PopularExperienceCardSkeleton />
-          <PopularExperienceCardSkeleton />
-          <PopularExperienceCardSkeleton />
-          <PopularExperienceCardSkeleton />
-        </div>
+    <SectionCarousel
+      title="Rehlatiuae Popular Experiences"
+      href="/popular-experiences"
+      label="See All"
+      status={status}
+      componentSkeleton={SkeletonCard}
+    >
+      {data?.data?.popularExperiencetrips?.map(
+        (data: PopularExperience, index: Key) => (
+          <CarouselItem
+            key={index}
+            className="relative md:basis-1/2 lg:basis-1/3 xl:basis-1/4 cursor-pointer"
+          >
+            <PopularExperienceCard data={data} />
+          </CarouselItem>
+        )
       )}
-      {status === "succeeded" && (
-        <Carousel
-          className="mt-10"
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          plugins={[
-            Autoplay({
-              delay: 3000,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            {data?.data?.popularExperiencetrips.map((data, index) => (
-              <CarouselItem
-                key={index}
-                className="relative md:basis-1/2 lg:basis-1/3 xl:basis-1/4 cursor-pointer"
-              >
-                <PopularExperienceCard data={data} />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      )}
-      {status === "failed" && (
-        <div className="h-[200px] flex justify-center items-center">
-          <p className="text-muted-foreground flex items-center">
-            <TriangleAlert className="h-5 w-5 mr-2" />
-            No result
-          </p>
-        </div>
-      )}
-    </div>
+    </SectionCarousel>
   );
 };
