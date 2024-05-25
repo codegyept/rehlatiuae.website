5cf24895e89c469d508406642a14662fe1b1084e
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-26 02:15:07 +0300

    update best-offers-trip section

--- app/(root)/(routes)/(home)/_components/best-offers-trips.tsx
+++ app/(root)/(routes)/(home)/_components/best-offers-trips.tsx
@@ -5,11 +5,11 @@ import { RootState } from "@/lib/store";
 import { useSelector } from "react-redux";
 
 import { BestOffersTripCard } from "@/components/card/best-offers-trip-card";
+import { SkeletonCard } from "@/components/card/skeleton-card";
 import { SectionCarousel } from "@/components/carousel/section-carousel";
 import { CarouselItem } from "@/components/ui/carousel";
 
 import { BestOffersTrip } from "@/interface";
-import { SkeletonCard } from "@/components/card/skeleton-card";
 
 export const BestOffersTrips = () => {
   const { data, status } = useSelector((state: RootState) => state.data);
@@ -22,7 +22,7 @@ export const BestOffersTrips = () => {
       status={status}
       componentSkeleton={SkeletonCard}
     >
-      {data?.data?.bestTrips?.map((data: BestOffersTrip, index: Key) => (
+      {data?.data?.bestOffers?.map((data: BestOffersTrip, index: Key) => (
         <CarouselItem
           key={index}
           className="relative md:basis-1/2 lg:basis-1/3 xl:basis-1/4 cursor-pointer"
