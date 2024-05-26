d9026ce1ec5f218ad93eb189322488dcca0cef6d
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-26 03:32:49 +0300

    update blogs section and blog-card component.

--- app/(root)/(routes)/(home)/_components/blogs.tsx
+++ app/(root)/(routes)/(home)/_components/blogs.tsx
@@ -1,74 +1,34 @@
 "use client";
 
-import Link from "next/link";
-import Autoplay from "embla-carousel-autoplay";
-
+import { Key } from "react";
 import { RootState } from "@/lib/store";
 import { useSelector } from "react-redux";
-import { ArrowUpRight, TriangleAlert } from "lucide-react";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
 import { BlogCard, BlogCardSkeleton } from "@/components/card/blog-card";
-import { Button } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
+import { SectionCarousel } from "@/components/carousel/section-carousel";
+import { CarouselItem } from "@/components/ui/carousel";
+
+import { Blog } from "@/interface";
 
 export const Blogs = () => {
   const { data, status } = useSelector((state: RootState) => state.data);
 
   return (
-    <div className="py-10 container mx-auto">
-      <Heading title="Recent Blogs">
-        <Button className="px-0 tracking-wide" variant="link" asChild>
-          <Link href="/blogs">
-            see all <ArrowUpRight className="h-5 w-5 ml-2" />
-          </Link>
-        </Button>
-      </Heading>
-      {status === "loading" && (
-        <div className="mt-10 grid grid-flow-col grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-5">
-          <BlogCardSkeleton />
-          <BlogCardSkeleton />
-          <BlogCardSkeleton />
-          <BlogCardSkeleton />
-        </div>
-      )}
-      {status === "succeeded" && (
-        <Carousel
-          className="mt-10"
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          plugins={[
-            Autoplay({
-              delay: 3000,
-            }),
-          ]}
+    <SectionCarousel
+      title="Recent Blogs"
+      href="/blogs"
+      label="See All"
+      status={status}
+      componentSkeleton={BlogCardSkeleton}
+    >
+      {data?.data?.blogs?.map((data: Blog, index: Key) => (
+        <CarouselItem
+          key={index}
+          className="relative md:basis-1/2 lg:basis-1/3 xl:basis-1/4 cursor-pointer"
         >
-          <CarouselContent>
-            {data?.data?.blogs.map((data, index) => (
-              <CarouselItem
-                key={index}
-                className="relative md:basis-1/2 lg:basis-1/3 xl:basis-1/4 cursor-pointer"
-              >
-                <BlogCard data={data} />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      )}
-      {status === "failed" && (
-        <div className="h-[200px] flex justify-center items-center">
-          <p className="text-muted-foreground flex items-center">
-            <TriangleAlert className="h-5 w-5 mr-2" />
-            No result
-          </p>
-        </div>
-      )}
-    </div>
+          <BlogCard data={data} />
+        </CarouselItem>
+      ))}
+    </SectionCarousel>
   );
 };
--- components/card/blog-card.tsx
+++ components/card/blog-card.tsx
@@ -26,25 +26,28 @@ export const BlogCard = ({ data }: BlogCardProps) => {
           alt={data.name}
           width={1000}
           height={1000}
-          className="object-cover"
+          className="object-cover w-full h-full"
+          onError={(e) => {
+            e.currentTarget.src = "/image-error.jpg";
+          }}
         />
-        <div className="hover:bg-black/50 duration-300 ease-in-out absolute bottom-0 left-0 w-full h-full" />
+        <div className="hover:bg-black/20 transition-all absolute bottom-0 left-0 w-full h-full" />
       </CardHeader>
       <CardContent className="py-5 space-y-3">
-        <h3 className="text-sm font-semibold">{data.name}</h3>
-        <p className="truncate">{data.description}</p>
+        <h3 className="text-lg font-semibold tracking-wider">{data.name}</h3>
+        <p className="text-sm text-muted-foreground tracking-wider truncate">
+          {data.description}
+        </p>
       </CardContent>
       <CardFooter className="pb-2 flex justify-between items-center">
         <Button asChild variant="ghost" size="sm">
           <Link href={data.name}>
-            {data.reviewAverage}
-            <Eye className="h-4 w-4 ml-2" />
+            {data.reviewAverage} <Eye className="h-4 w-4 ml-2" />
           </Link>
         </Button>
         <Button asChild variant="link" size="sm">
           <Link href={data.name}>
-            Read more
-            <ArrowUpRight className="h-4 w-4 ml-2" />
+            Read more <ArrowUpRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
       </CardFooter>
@@ -54,10 +57,19 @@ export const BlogCard = ({ data }: BlogCardProps) => {
 
 export const BlogCardSkeleton = () => {
   return (
-    <div className="space-y-3">
-      <Skeleton className="h-[250px] rounded-md" />
-      <Skeleton className="h-[18px] rounded-md" />
-      <Skeleton className="w-2/3 h-[18px] rounded-md" />
-    </div>
+    <Card className="overflow-hidden">
+      <CardHeader className="relative p-0 overflow-hidden h-[300px]">
+        <Skeleton className="w-full h-full" />
+      </CardHeader>
+      <CardContent className="py-5 space-y-3">
+        <Skeleton className="w-3/4 h-6 mb-2" />
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-2/3 h-4" />
+      </CardContent>
+      <CardFooter className="pb-2 flex justify-between items-center">
+        <Skeleton className="h-6 w-12" />
+        <Skeleton className="h-6 w-16" />
+      </CardFooter>
+    </Card>
   );
 };
