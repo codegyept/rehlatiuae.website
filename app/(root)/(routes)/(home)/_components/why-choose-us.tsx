b2d4242deb61bece172a6a745a011f6c923a162f
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-26 03:16:57 +0300

    update why-choose-us section and feature-card component.

--- app/(root)/(routes)/(home)/_components/why-choose-us.tsx
+++ app/(root)/(routes)/(home)/_components/why-choose-us.tsx
@@ -6,40 +6,40 @@ import { FeatureCard } from "@/components/card/feature-card";
 
 export const WhyChooseUs = () => {
   return (
-    <div className="py-10 container mx-auto">
-      <div className="w-full flex flex-col gap-y-2">
-        <h1 className="text-lg md:text-xl lg:text-2xl tracking-wider flex-1">
-          Why Choose Us?
-        </h1>
-        <p className="tracking-wide text-sm text-muted-foreground leading-8">
-          We will confidently explain it to you with complete confidence and
-          excellence!
-        </p>
-      </div>
-      <div className="mt-10 flex items-start justify-between gap-10 flex-col lg:flex-row">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-          {features.map(({ id, title, icon, description }) => (
-            <FeatureCard
-              key={id}
-              title={title}
-              description={description}
-              icon={icon}
+    <section className="py-20 container mx-auto">
+      <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-16">
+        <div>
+          <div className="text-center lg:text-left mb-8">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
+              Why Choose Us?
+            </h1>
+            <p className="text-sm md:text-base text-muted-foreground">
+              We will confidently explain it to you with complete confidence and
+              excellence!
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {features.map(({ id, title, icon, description }) => (
+              <FeatureCard
+                key={id}
+                title={title}
+                description={description}
+                icon={icon}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="relative w-full max-w-md lg:max-w-[550px] h-[400px] lg:h-[600px]">
+          <div className="w-full h-full rounded-3xl overflow-hidden shadow-lg transition-transform duration-300 ease-in-out transform lg:hover:scale-110 lg:rotate-12 hover:rotate-0">
+            <Image
+              src={whyChooseUs}
+              alt="Why Choose Us"
+              layout="fill"
+              objectFit="cover"
             />
-          ))}
-        </div>
-        <div className="w-full lg:max-w-[550px] h-[600px] lg:h-full">
-          <Image
-            src={whyChooseUs}
-            alt="Why Choose Us"
-            className="hidden lg:block w-full h-full object-cover rotate-6 hover:rotate-0 duration-300 ease-in-out cursor-pointer scale-90 hover:scale-105 hover:shadow-lg rounded-3xl"
-          />
-          <Image
-            src={whyChooseUs}
-            alt="Why Choose Us"
-            className="block lg:hidden w-full h-full object-cover rounded-3xl"
-          />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
--- components/card/feature-card.tsx
+++ components/card/feature-card.tsx
@@ -1,5 +1,4 @@
 import { LucideIcon } from "lucide-react";
-
 import {
   Card,
   CardDescription,
@@ -8,27 +7,25 @@ import {
 } from "@/components/ui/card";
 
 interface FeatureCardProps {
-  icon: LucideIcon;
   title: string;
+  icon: LucideIcon;
   description: string;
 }
 
 export const FeatureCard = ({
-  icon: Icon,
   title,
+  icon: Icon,
   description,
 }: FeatureCardProps) => {
   return (
     <Card className="border-none shadow-none px-0">
-      <CardHeader className="px-0">
-        <div className="bg-blue/20 w-12 h-12 rounded-full relative mb-5">
-          <Icon className="h-10 w-10 absolute top-2 left-5" />
-        </div>
-        <CardTitle className="tracking-wide text-xl">{title}</CardTitle>
-        <CardDescription className="leading-8 tracking-wide">
-          {description}
-        </CardDescription>
+      <CardHeader className="px-0 bg-blue/25 rounded-full w-[50px] h-[50px] mb-5 relative">
+        <Icon className="h-10 w-10 absolute left-6 bottom-0" />
       </CardHeader>
+      <CardTitle className="tracking-wide text-xl">{title}</CardTitle>
+      <CardDescription className="leading-8 tracking-wide">
+        {description}
+      </CardDescription>
     </Card>
   );
 };
